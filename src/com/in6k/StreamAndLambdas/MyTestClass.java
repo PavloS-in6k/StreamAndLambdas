5dcce0e31ebe194b902c249ddcaefdbab928d2a4
Pavlo-in6k <pavlos@interlink-ua.com>
2016-07-12 14:37:17 +0300

    Still not working version, stopped work acording to review code task.

--- src/com/in6k/StreamAndLambdas/MyTestClass.java
+++ src/com/in6k/StreamAndLambdas/MyTestClass.java
@@ -1,18 +1,53 @@
 package com.in6k.StreamAndLambdas;
 
-import sun.nio.cs.Surrogate;
+import sun.security.pkcs11.wrapper.Functions;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.Year;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
 public class MyTestClass {
-    public static List<LocalDate> getDays(Year year) {
-        List <LocalDate> dates = new ArrayList<LocalDate>();
-        Stream.iterate()
-        dates.stream().filter(value -> value.getYear())
-        return dates;
+    public static Stream getUnsortedStreamDays(Year year) {
+        Stream stream = Stream.iterate(LocalDate.parse("2016-01-01"), value -> value.plusDays(1))
+                .limit(year.length());
+        return stream;
+    }
+
+    public static Stream getWeekends(Stream<LocalDate> stream) {
+        return stream
+                .filter(value -> isDateValid(value));
+    }
+
+    public static Map<Month, List<LocalDate>> getMapOfDaysFromStream(Stream<LocalDate> stream) {
+        //return stream.collect(Collectors.groupingBy(value -> value.getMonth(), value -> value));
+        return stream
+                .collect(toMap(value -> value.getMonth(), value -> value))
+                .entrySet()
+                .stream().collect(Collectors.toMap());
+
+
+    }
+
+    private static boolean isDateValid(LocalDate value) {
+        return (value.getMonth().equals(Month.FEBRUARY) && value.getDayOfWeek().equals(DayOfWeek.MONDAY)) ?
+                (value.getDayOfMonth() < 8 || (value.getDayOfMonth() > (value.getMonth().maxLength() / 7) * 4))
+                        && isWeekend(value.getDayOfWeek())
+                :
+                (value.getDayOfMonth() < 8 || (value.getDayOfMonth() > (value.getMonth().maxLength() / 7) * 5))
+                        && isWeekend(value.getDayOfWeek());
+    }
+
+    private static boolean isWeekend(DayOfWeek DOW) {
+        return (DOW.equals(DayOfWeek.SUNDAY) || DOW.equals(DayOfWeek.SATURDAY)) ? true : false;
     }
 }
--- test/com/in6k/StreamAndLambdas/GeneralTests.java
+++ test/com/in6k/StreamAndLambdas/GeneralTests.java
@@ -1,25 +1,113 @@
 package com.in6k.StreamAndLambdas;
 
 
-import org.junit.Assert;
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.Year;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.stream.Stream;
 
 import static com.in6k.StreamAndLambdas.MyTestClass.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class GeneralTests {
 
-    @Test
-    public void isNumberOfDaysIsCorrect() throws Exception {
-        Year year = Year.parse("2016");
-        assertEquals(getDays(Year.parse("2016")).size(),year.length()-1);
+    Year year;
+
+    @Before
+    public void setup() {
+        year = Year.parse("2016");
     }
 
+    @Test
+    public void isNumberOfDaysIsCorrect() throws Exception {
+        assertEquals(year.length(), getUnsortedStreamDays(year).count());
+    }
 
+    @Test
+    public void isFistDayInStreamIsFirstDayOfYear() throws Exception {
+        assertEquals(LocalDate.parse("2016-01-01"), getUnsortedStreamDays(year).toArray()[0]);
+    }
+
+    @Test
+    public void isWrongDatesFiltered() throws Exception {
+        Stream stream = getWeekends(Stream.of(
+                LocalDate.parse("2016-01-01"),
+                LocalDate.parse("2016-01-04"),
+                LocalDate.parse("2016-01-05"),
+                LocalDate.parse("2016-01-06"),
+                LocalDate.parse("2016-01-07"),
+                LocalDate.parse("2016-01-08")
+        ));
+        assertThat(stream.toArray().length, is(0));
+    }
+
+    @Test
+    public void isFirstSortedDatesDayIsWeekend() throws Exception {
+        Stream stream = getWeekends(Stream.of(
+                LocalDate.parse("2016-01-01"),
+                LocalDate.parse("2016-01-02"),
+                LocalDate.parse("2016-01-03"),
+                LocalDate.parse("2016-01-04")
+        ));
+        ArrayList<Object> dates = new ArrayList<>();
+        dates.add(LocalDate.parse("2016-01-02"));
+        dates.add(LocalDate.parse("2016-01-03"));
+        assertThat(stream.toArray(), equalTo(dates.toArray()));
+    }
+
+    @Test
+    public void isLastDatesIsWeekends() throws Exception {
+        Stream stream = getWeekends(Stream.of(
+                LocalDate.parse("2016-01-29"),
+                LocalDate.parse("2016-01-30"),
+                LocalDate.parse("2016-01-31")
+        ));
+        ArrayList<Object> dates = new ArrayList<>();
+        dates.add(LocalDate.parse("2016-01-30"));
+        dates.add(LocalDate.parse("2016-01-31"));
+        assertThat(stream.toArray(), equalTo(dates.toArray()));
+    }
+
+    @Test
+    public void isWrongDatesIgnored() throws Exception {
+        Stream stream = getWeekends(Stream.of(
+                LocalDate.parse("2016-01-15"),
+                LocalDate.parse("2016-01-16"),
+                LocalDate.parse("2016-01-17"),
+                LocalDate.parse("2016-01-18")
+        ));
+        ArrayList<Object> dates = new ArrayList<>();
+        assertThat(stream.toArray(), equalTo(dates.toArray()));
+    }
+
+    @Test
+    public void isMapReturnedRight() throws Exception {
+        Stream stream = getWeekends(Stream.of(
+                LocalDate.parse("2016-01-29"),
+                LocalDate.parse("2016-01-30"),
+                LocalDate.parse("2016-01-31")
+        ));
+        Map<Month, LocalDate> dates = new HashMap<>();
+
+        dates.put(Month.JANUARY, LocalDate.parse("2016-01-30"));
+        dates.put(Month.JANUARY, LocalDate.parse("2016-01-31"));
+
+
+
+        assertThat(getMapOfDaysFromStream(stream), equalTo(dates));
+    }
 
 
 }
